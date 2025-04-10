0ec06dae0fcb60a102ab14783e833eacc9538e1a
Pedro Lucas Oliveira <148586318+PedroLucas-O@users.noreply.github.com>
2025-04-10 19:59:26 -0300

    Add Script By Upload

---
+++ script.js
@@ -0,0 +1,12 @@
+
+
+const pergunta1 = "Você gosta de programar?";
+const pergunta2 = "Você já estudou JavaScript antes?";
+const pergunta3 = "Você pretende praticar todos os dias?";
+const resposta1 = prompt(pergunta1);
+const resposta2 = prompt(pergunta2);
+const resposta3 = prompt(pergunta3);
+
+console.log(pergunta1 + " - " + resposta1);
+console.log(pergunta2 + " - " + resposta2);
+console.log(pergunta3 + " - " + resposta3);
